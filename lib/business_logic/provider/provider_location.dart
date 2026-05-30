4970aaa5e65161432c4d09f31150ac919a8169c7
addis-ale <addisale0727@gmail.com>
2026-05-30 11:26:00 +0300

    Document app-wide location provider

--- lib/business_logic/provider/provider_location.dart
+++ lib/business_logic/provider/provider_location.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
+// Holds the latest device Position app-wide; call notifyListeners after
+// reassigning `position` to refresh dependent widgets.
 class ProviderLocation extends ChangeNotifier {
   Position? position;
   ProviderLocation({
